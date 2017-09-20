0a0f7c4a99145b0999d75b11c378c5de71c988b9
hellsdark <hellsdark@gmail.com>
2017-09-20 09:03:34 +0200

    update

--- api.js
+++ api.js
@@ -65,19 +65,3 @@ function getCvParagraphs(){
     })   
 }
 
-var cv=new Vue({
-    el:"#cv",
-    data:{
-        workExperiences:[{fields:{title:''}}],
-        paragraphs:{}
-    },
-    created:function(){
-    this.getData();
-    },
-    methods:{
-        getData(){
-            getWorkExperiences().then(response => this.workExperiences=response);
-            getCvParagraphs().then(response => this.paragraphs=response);
-        }
-    }
-});
