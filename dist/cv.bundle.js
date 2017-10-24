28d1d045c25116b6c67f1169d208fd34842fe5f2
hellsdark <hellsdark@gmail.com>
2017-10-24 15:52:54 +0200

    webpacking

--- dist/cv.bundle.js
+++ dist/cv.bundle.js
@@ -60,12 +60,12 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 11);
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
 /******/ })
 /************************************************************************/
-/******/ ({
-
-/***/ 11:
+/******/ ([
+/* 0 */,
+/* 1 */
 /***/ (function(module, exports) {
 
 
@@ -88,5 +88,4 @@ var cv=new Vue({
 
 
 /***/ })
-
-/******/ });
+/******/ ]);
