67bf89a44b55e86cf797bdb26220ca58f32e056f
hellsdark <hellsdark@gmail.com>
2017-11-06 23:22:30 +0100

    uglify js bundles

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,34 @@
-const path = require('path');
+const path = require("path");
+const webpack = require("webpack");
 
 const config = {
-    entry: {
-        blog:  './blog.js',
-        cv:  './cv.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js'
-    }
+  entry: {
+    blog: "./blog.js",
+    cv: "./cv.js"
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js"
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      uglifyOptions: { ecma: 8, sourceMap: true, comments: false }
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  }
 };
 
 module.exports = config;
