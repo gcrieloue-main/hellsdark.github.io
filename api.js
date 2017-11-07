2965df06a451c0ceb6344b513f1f961b70421162
hellsdark <hellsdark@gmail.com>
2017-11-07 11:33:19 +0100

    add smileys to blog

--- api.js
+++ api.js
@@ -1,76 +1,93 @@
-import * as showdown from './showdown.min.js';
-import * as contentful from 'contentful';
+import * as showdown from "./showdown.min.js";
+import * as contentful from "contentful";
 
 const client = contentful.createClient({
-    // This is the space ID. A space is like a project folder in Contentful terms
-    space: "oamir411dfuu",
-    // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-    accessToken: "fe844e41216f12522cc40b8a179e7c81c8a0f17b797503155ba949afbb6aca96"
+  // This is the space ID. A space is like a project folder in Contentful terms
+  space: "oamir411dfuu",
+  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+  accessToken:
+    "fe844e41216f12522cc40b8a179e7c81c8a0f17b797503155ba949afbb6aca96"
 });
 
-var converter = new showdown.Converter({headerLevelStart: 4, simpleLineBreaks: true});
+var converter = new showdown.Converter({
+  headerLevelStart: 4,
+  simpleLineBreaks: true
+});
 
-export function getArticles(page){
-    page = page || 1;
-    var nbArticles = 10;
-    return client.getEntries({
-        content_type: "article",
-        order: "-fields.date",
-        skip: (page - 1) * nbArticles,
-        limit: nbArticles
-        
+export function getArticles(page) {
+  page = page || 1;
+  var nbArticles = 10;
+  return client
+    .getEntries({
+      content_type: "article",
+      order: "-fields.date",
+      skip: (page - 1) * nbArticles,
+      limit: nbArticles
     })
-        .then((response) => response.items.map(function(element){
-            element.fields.content=converter.makeHtml(element.fields.content);
-            if (element.fields.date!=undefined){
-            var date = new Date(element.fields.date);
-            var formatedMonth = ("0" + (date.getMonth()+1)).slice(-2);
-            var formatedDay = ("0" + (date.getDate())).slice(-2);
-            element.fields.date=formatedDay+"/"+formatedMonth;
+    .then(response =>
+      response.items.map(function(element) {
+        element.fields.content = converter.makeHtml(replaceSmileys(element.fields.content));
+        if (element.fields.date != undefined) {
+          var date = new Date(element.fields.date);
+          var formatedMonth = ("0" + (date.getMonth() + 1)).slice(-2);
+          var formatedDay = ("0" + date.getDate()).slice(-2);
+          element.fields.date = formatedDay + "/" + formatedMonth;
         }
-        return element
-    }))
-        .catch((error) => {
-        console.error(error)
-    })   
+        return element;
+      })
+    )
+    .catch(error => {
+      console.error(error);
+    });
 }
 
-export function getWorkExperiences(){
-    return client.getEntries({
-        content_type: "workExperience",
-           order:"-fields.periodBegin"
+function replaceSmileys(text){
+    return text.replace(':)', '😃');
+}
+
+export function getWorkExperiences() {
+  return client
+    .getEntries({
+      content_type: "workExperience",
+      order: "-fields.periodBegin"
     })
-        .then((response) => response.items.map(function(element){
-            var begin = new Date(element.fields.periodBegin);
-            var end = new Date(element.fields.periodEnd);
-            var beginDate = ("0" + (begin.getMonth()+1)).slice(-2) + "/" 
-            + begin.getFullYear();
-        var endDate="Aujourd'hui";
-        if (end<new Date()){
-            endDate = ("0" + (end.getMonth()+1)).slice(-2) + "/" 
-            + end.getFullYear();
-            }
-            element.fields.begin=beginDate;
-            element.fields.end=endDate;
-        element.fields.description=converter.makeHtml(element.fields.description);
-        return element
-    }))
-        .catch((error) => {
-        console.error(error);
-    })   
+    .then(response =>
+      response.items.map(function(element) {
+        var begin = new Date(element.fields.periodBegin);
+        var end = new Date(element.fields.periodEnd);
+        var beginDate =
+          ("0" + (begin.getMonth() + 1)).slice(-2) + "/" + begin.getFullYear();
+        var endDate = "Aujourd'hui";
+        if (end < new Date()) {
+          endDate =
+            ("0" + (end.getMonth() + 1)).slice(-2) + "/" + end.getFullYear();
+        }
+        element.fields.begin = beginDate;
+        element.fields.end = endDate;
+        element.fields.description = converter.makeHtml(
+          element.fields.description
+        );
+        return element;
+      })
+    )
+    .catch(error => {
+      console.error(error);
+    });
 }
 
-export function getCvParagraphs(){
-    return client.getEntries({
-        content_type: "paragraph",
-        order:"sys.createdAt"
+export function getCvParagraphs() {
+  return client
+    .getEntries({
+      content_type: "paragraph",
+      order: "sys.createdAt"
     })
-        .then((response) => response.items.map(function(element){
-        element.fields.text=converter.makeHtml(element.fields.text);
-        return element
-    }))
-        .catch((error) => {
-        console.error(error)
-    })   
+    .then(response =>
+      response.items.map(function(element) {
+        element.fields.text = converter.makeHtml(element.fields.text);
+        return element;
+      })
+    )
+    .catch(error => {
+      console.error(error);
+    });
 }
-
