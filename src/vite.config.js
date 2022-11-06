c714ca7505ebe76a6341f357d0b28642f16a20d1
Crieloue Gilles <hellsdark@gmail.com>
2022-11-06 20:21:59 +0100

    Update vite.config.js

--- src/vite.config.js
+++ src/vite.config.js
@@ -4,6 +4,8 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 
 // Warning ! Build is currently broken, see https://github.com/vitejs/vite/issues/4890
+// https://github.com/vitejs/vite/issues/8992
+// https://github.com/vitejs/vite/issues/6375
 export default defineConfig({
   build: {
     rollupOptions: {
